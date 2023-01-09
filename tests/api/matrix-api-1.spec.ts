d6b882b63d46c53d843819c03ca71c14a3ba9498
janostgren <jan.ostgren@gmail.com>
2023-01-09 13:59:03 +0100

    API Test complete

--- tests/api/matrix-api-1.spec.ts
+++ tests/api/matrix-api-1.spec.ts
@@ -2,47 +2,55 @@ import { expect, APIRequestContext } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 import { test } from "../../lib/fixtures/apiProfilesFixture";
 import { MatrixApiClient } from "../../lib/apiModels/matrixApiClient";
+import { MattermostApiClient } from "../../lib/apiModels/mattermostApiClient";
 
 test.describe("Matrix - API Send message", () => {
   let matrixContext: APIRequestContext;
   let matrixApiClient: MatrixApiClient;
+  let mmContext: APIRequestContext;
+  let mmApiClient: MatrixApiClient;
+  let roomName: string = "town-square";
+  let hackerPhrase=faker.hacker.phrase()
 
-  test.beforeAll(async ({ playwright, matrix_a }) => {
+  test.beforeAll(async ({ playwright, matrix_a, admin_mm }) => {
     matrixApiClient = new MatrixApiClient(matrix_a, playwright.request);
     matrixContext = await matrixApiClient.authenticate();
-  })
-  
+    mmApiClient = new MattermostApiClient(admin_mm, playwright.request);
+    mmContext = await mmApiClient.authenticate();
+  });
 
   test("Send Text message", async ({}) => {
-    let publicRooms = await test.step("Get Public rooms", async () => {
+    let publicRooms: any[] = await test.step("Get Public rooms", async () => {
       const rooms = await matrixContext.get(
-        `/_matrix/client/r0/publicRooms`,
+        `/_matrix/client/r0/publicRooms?limit=100`,
         {}
       );
-      let t = rooms.statusText();
       expect(rooms.ok()).toBeTruthy();
       let json = await rooms.json();
       console.log(json);
       return json.chunk;
     });
 
-    await test.step("Get the room", async () => {
-      let domain = matrixApiClient.getDomain();
-      let roomAlias = encodeURIComponent(`#town-square:${domain}`);
+    let theRoom =
+      await test.step(`Get the public rooms: ${roomName} `, async () => {
+        expect(
+          publicRooms.length,
+          "More than one public room expected."
+        ).toBeGreaterThanOrEqual(2);
+        let roomAlias = `#${roomName}:${matrixApiClient.getDomain()}`;
+        let theRoom = publicRooms.find((room) => {
+          return room.canonical_alias === roomAlias;
+        });
+        expect(theRoom).not.toBeUndefined();
+        return theRoom;
+      });
 
-      let p = `/_matrix/client/r0/directory/room/${roomAlias}`;
-      let apiResponse = await matrixContext.get(p, {});
-      expect(apiResponse.ok()).toBeTruthy();
-      let json = await apiResponse.json();
-      let roomId = json.room_id;
-      expect(roomId).not.toBeUndefined();
-    });
-
-    await test.step("Send Text Message", async () => {
+    
+    let theMessage=await test.step("Send Text Message", async () => {
       const transactionId = "m." + Date.now();
-      let message = `**Strange message seen:** \n${faker.hacker.phrase()}\n from ${faker.internet.email()}`;
+      let message = `Strange message seen:\n${hackerPhrase}\n from ${faker.internet.email()}`;
 
-      let p = `/_matrix/client/r0/rooms/${roomId}/send/m.room.message/${transactionId}`;
+      let p = `/_matrix/client/r0/rooms/${theRoom.room_id}/send/m.room.message/${transactionId}`;
       let apiResponse = await matrixContext.put(p, {
         data: {
           body: message,
@@ -52,6 +60,44 @@ test.describe("Matrix - API Send message", () => {
       let statusText = apiResponse.statusText();
       expect(apiResponse.ok()).toBeTruthy();
       let json = await apiResponse.json();
+      return message
+    });
+    
+
+    let theChannel =
+      await test.step("Get The channel from Mattermost", async () => {
+        const channels = await mmContext.get(
+          `/api/v4/users/me/channels?page=0&per_page=200`,
+          {}
+        );
+        expect(channels.ok()).toBeTruthy();
+        let json = await channels.json();
+        let theChannel = json.find((channel) => {
+          return channel.name === roomName;
+        });
+        expect(theChannel).toBeDefined();
+        return theChannel;
+      });
+
+    await test.step("Get channel messages from Mattermost", async () => {
+      let since: number = Date.now() - 3600 * 1000;
+     
+      const apiResponse = await mmContext.get(
+        `/api/v4/channels/${theChannel.id}/posts?page=0&per_page=200&since=${since}`,
+        {}
+      );
+      expect(apiResponse.ok()).toBeTruthy();
+      let json = await apiResponse.json();
+     
+      expect(json.order.length,"Posts found").toBeGreaterThanOrEqual(1)
+      let posts:any[] =Object.values(json.posts)
+      let thePost=posts.find (post => {
+        return post.message.includes(hackerPhrase) 
+      })
+      expect(thePost,`Post not found with message ${hackerPhrase} in channel ${roomName}`).toBeDefined()
+
+      
+      return 1
     });
   });
-
+});
